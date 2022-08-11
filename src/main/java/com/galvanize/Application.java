3d238657fa358bb2b328491f38a24520b33b9ed2
kwisatz-haderachs <frcranmer@gmail.com>
2022-08-11 10:45:51 -0500

    added commandline args

--- src/main/java/com/galvanize/Application.java
+++ src/main/java/com/galvanize/Application.java
@@ -6,15 +6,17 @@ import java.lang.Math;
 public class Application {
     public static void main(String[] args){
         System.out.println("Hello World!");
-
+        if(args[0].isEmpty() == false){
+            caeserCipher(args);
+        }
     }
 
-    public void ceaser_cipher(){
+    public static void caeserCipher(String[] args){
         Scanner sc = new Scanner(System.in);
         int rand = (int) (Math.random() * 20) + 1;
         System.out.println("Enter text to cipher: ");
-        String s = sc.nextLine();
-        Encrypt cipher = new Encrypt(s, rand);
+        //String s = sc.nextLine();
+        Encrypt cipher = new Encrypt(args[0], rand);
         String ci = cipher.encryption();
         System.out.println(ci);
     }
