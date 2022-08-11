d69a69763475f221615eec705a6de7fbbeae4802
kwisatz-haderachs <frcranmer@gmail.com>
2022-08-11 11:56:09 -0500

    CLI bonus work added

--- src/main/java/com/galvanize/Application.java
+++ src/main/java/com/galvanize/Application.java
@@ -6,17 +6,60 @@ import java.lang.Math;
 public class Application {
     public static void main(String[] args){
         System.out.println("Hello World!");
-        if(args[0].isEmpty() == false){
-            caeserCipher(args);
+
+        if(args.length == 0) {
+            System.out.println("No arguments provided");
+            caeserCipher();
+        } else {
+            try {
+
+                boolean c = check30(args);
+                if (c == true) System.out.println("The input included the number 30");
+                else System.out.println("The input did not include the number 30");
+
+                int x = checkPositive(args);
+                if (x >= 1) System.out.println("The input included some negative numbers or zero");
+                else System.out.println("The input included only positive numbers");
+
+
+            } catch (Exception e) {
+                System.out.println("");
+            }
         }
     }
 
-    public static void caeserCipher(String[] args){
+    /*
+
+    Bonus challenge for the above prompts (only try this if you complete both): Handle the case where input is provided that cannot be converted to an int.
+    (Hint: you will need some way to “catch” invalid input).
+     */
+
+    public static boolean check30(String[] args){
+        for (int i = 0; i < args.length; i++) {
+            if(Integer.parseInt(args[i]) == 30){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static int checkPositive(String[] args){
+        int neg = 0;
+        for (int i = 0; i < args.length; i++) {
+            if (Integer.parseInt(args[i]) <= 0) {
+                neg++;
+            }
+        }
+        if (neg >= 1) return neg;
+        return 0;
+    }
+
+    public static void caeserCipher(){
         Scanner sc = new Scanner(System.in);
         int rand = (int) (Math.random() * 20) + 1;
         System.out.println("Enter text to cipher: ");
-        //String s = sc.nextLine();
-        Encrypt cipher = new Encrypt(args[0], rand);
+        String s = sc.nextLine();
+        Encrypt cipher = new Encrypt(s, rand);
         String ci = cipher.encryption();
         System.out.println(ci);
     }
